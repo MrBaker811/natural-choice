d06ed16da9425932546e2154192d97dace4cfdb9
Sean Baker <musiq365@yahoo.com>
2016-09-16 12:22:27 -0400

    added devise for authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+#remeber to change one the production url is finalized
+   config.action_mailer.default_url_options = { host: 'http://thenaturalchoice.herokuapp.com/', port: 3000 }
+
 end
